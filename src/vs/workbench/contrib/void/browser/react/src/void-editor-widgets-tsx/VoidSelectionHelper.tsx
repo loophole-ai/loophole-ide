e3990e00a1fd09b217d55baf0e28f1f230fcbf38
Garv Agnihotri <garvagnihotri22@gmail.com>
2026-09-12 07:30:18 +0530

    replace @@void-scope with @@loophole-scope (VoidSelectionhelper)

--- src/vs/workbench/contrib/void/browser/react/src/void-editor-widgets-tsx/VoidSelectionHelper.tsx
+++ src/vs/workbench/contrib/void/browser/react/src/void-editor-widgets-tsx/VoidSelectionHelper.tsx
@@ -20,7 +20,7 @@ export const VoidSelectionHelperMain = (props: VoidSelectionHelperProps) => {
 	const isDark = useIsDark()
 
 	return <div
-		className={`@@void-scope ${isDark ? 'dark' : ''}`}
+		className={`@@loophole-scope ${isDark ? 'dark' : ''}`}
 	>
 		<VoidSelectionHelper {...props} />
 	</div>
