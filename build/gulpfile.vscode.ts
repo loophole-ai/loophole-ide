fa1b56d6d9dec6ce040304e94b5e0b011266a052
Garv Agnihotri <garvagnihotri22@gmail.com>
2026-09-18 08:42:15 +0530

    stamp loopholeVersion into exe PE version metadata

--- build/gulpfile.vscode.ts
+++ build/gulpfile.vscode.ts
@@ -642,7 +642,8 @@ function patchWin32DependenciesTask(destinationFolderName: string) {
 		])).flatMap(o => o);
 		const packageJson = JSON.parse(await fs.promises.readFile(path.join(cwd, versionedResourcesFolder, 'resources', 'app', 'package.json'), 'utf8'));
 		const product = JSON.parse(await fs.promises.readFile(path.join(cwd, versionedResourcesFolder, 'resources', 'app', 'product.json'), 'utf8'));
-		const baseVersion = packageJson.version.replace(/-.*$/, '');
+		const displayVersion = product.loopholeVersion ?? packageJson.version;
+		const baseVersion = displayVersion.replace(/-.*$/, '');
 
 		const patchPromises = deps.map<Promise<unknown>>(async dep => {
 			const basename = path.basename(dep);
@@ -653,12 +654,12 @@ function patchWin32DependenciesTask(destinationFolderName: string) {
 					'version-string': {
 						'CompanyName': 'Microsoft Corporation',
 						'FileDescription': product.nameLong,
-						'FileVersion': packageJson.version,
+						'FileVersion': displayVersion,
 						'InternalName': basename,
 						'LegalCopyright': 'Copyright (C) 2026 Microsoft. All rights reserved',
 						'OriginalFilename': basename,
 						'ProductName': product.nameLong,
-						'ProductVersion': packageJson.version,
+						'ProductVersion': displayVersion,
 					}
 				});
 			} catch (err) {
