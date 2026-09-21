58f4151325a0bacd4b8def7f5ed0c59bd71600ba
Garv Agnihotri <garvagnihotri22@gmail.com>
2026-09-21 11:23:22 +0530

    rename Void to Loophole types

--- src/vs/workbench/contrib/void/common/voidUpdateServiceTypes.ts
+++ src/vs/workbench/contrib/void/common/voidUpdateServiceTypes.ts
@@ -3,7 +3,7 @@
  *  Licensed under the Apache License, Version 2.0. See LICENSE.txt for more information.
  *--------------------------------------------------------------------------------------*/
 
-export type VoidCheckUpdateRespose = {
+export type LoopholeCheckUpdateRespose = {
 	message: string,
 	action?: 'reinstall' | 'restart' | 'download' | 'apply'
 } | {
@@ -11,11 +11,9 @@ export type VoidCheckUpdateRespose = {
 	actions?: undefined,
 } | null
 
-export interface IVoidUpdateInfo {
+export interface ILoopholeUpdateInfo {
 	version?: string;
 	assetName?: string;
 	isDownloaded?: boolean;
 	isReady?: boolean;
 }
-
-
